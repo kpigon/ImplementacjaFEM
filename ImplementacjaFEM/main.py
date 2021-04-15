0168ee9419ac0a5e3a70d9950cd3725ffd03e34d
kpigon <devo4050@gmail.com>
2021-04-15 16:03:51 +0200

    Dodaj pliki projektów.

---
+++ ImplementacjaFEM/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+wezly=np.array([[1,0],
+                [2,1],
+                [3,0.5],
+                [4,0.75]])
+
+elementy=np.array([[1,1,3],
+                   [2,4,2],
+                   [3,3,4]])
